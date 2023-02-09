8fe45952986e47334862cdc44fb9bd4c03b4c617
Victoria Burtovska <viktoriaburtovskaa@gmail.com>
2023-02-09 21:28:50 +0100

    Add homework file

--- homeworks/hw_27_01_23.js
+++ homeworks/hw_27_01_23.js
@@ -1,4 +1,5 @@
-// Задан объект someObj с произвольными сво-ми. Напишите функцию getKeys(), которая в качестве аргумента получает объект и возвращает результат работы метода Object.keys() согласно примеру:
+// Задан объект someObj с произвольными сво-ми. Напишите функцию getKeys(), 
+//которая в качестве аргумента получает объект и возвращает результат работы метода Object.keys() согласно примеру:
 // Пример:
  let someObjj = {
  key1: 'value1',
@@ -7,27 +8,64 @@
  key4: 'value4',
  }
 
+ function getKeys(obj) {
+    let keys = [];
+    for (const key in obj) {
+        keys.push(key)
+    }
+    return keys;
+}
+
+console.log(getKeys(someObjj));
+
 // Результат: ['key1','key2','key3','key4']
 
 // Создайте функцию getAvg(), которая получает в качестве аргумента массив и возвращает среднее значение среди всех элементов. Примечание: гарантируется, что все элементы передают числовое значение
 // Пример: [1,2,3,4,5]
-
+    function getAvg(arr) {
+    return arr.reduce((avg, el) => avg + el, 0) / arr.length;
+    }
+console.log(getAvg([1,2,3,4,5]));
 // Результат: 3
 
 
 // Создайте функцию countString(), которая получает массив в качестве аргумента.
 // Реализуйте функционал, который посчитает и выведет в консоль количество элементов, чей тип равен ‘string’
-// Пример: [1,true,'3','value1',9,'key']
+//Пример: 
+let arr = [1,true,'3','value1',9,'key']
+
+    function countString(arr) {
+       let count = 0;
+       arr.forEach(el => {
+        if (typeof el == 'string') {
+        count++
+        }
+       });
+       console.log(count); 
+    }
+  countString(arr)
 // Результат: 3
 
-// Задан объект someObj с произвольными сво-ми. Напишите функцию getEntries(), которая в качестве аргумента получает объект и возвращает результат работы метода Object.entries() согласно примеру:
+// Задан объект someObj с произвольными сво-ми. 
+//Напишите функцию getEntries(), которая в качестве аргумента получает объект 
+//и возвращает результат работы метода Object.entries() согласно примеру:
+
 // Пример:
-// let someObj = {
-// key1: 'value1',
-// key2: 'value2',
-// key3: 'value3',
-// key4: 'value4',
-// }
+ let someObj = {
+ key1: 'value1',
+ key2: 'value2',
+ key3: 'value3',
+ key4: 'value4',
+}
+
+function getEntries(obj){
+    let arrEntries = [];
+   for (const key in obj) {
+    arrEntries.push([key, obj[key]])
+   }
+  return arrEntries;
+}
+console.log(getEntries(someObj)); 
 // Примечание: Использовать класс Object нельзя
 
 // Результат: [['key1', 'value1'],['key2', 'value2'],['key3', 'value3'],['key4', 'value4']]
@@ -38,8 +76,14 @@
 // Количество сво-тв должно быть равным количеству элементов массива
 
 	
-// Пример: [1,true,'3','value1',9,'key']
+// Пример: 
+const array = [1,true,'3','value1',9,'key'];
+const obj = {};
 
+array.forEach((el,i) => {
+	obj[`key${+i + 1}`] = el;
+});
+console.log(obj);
 
 
 // 	Результат: 
